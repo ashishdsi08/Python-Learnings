467dbf6e107a04d9d400d6b7c253602dcf09a2f3
ashishdsi08 <32002632+ashishdsi08@users.noreply.github.com>
2024-03-30 10:17:53 +0530

    Hello World

---
+++ Docs/HandsOn-Labs/1 HelloWorld.py
@@ -0,0 +1,33 @@
+#Execute .py file from command line
+#Execute statements quickly on the interactive IDLE shell window
+#Learn about the Python Indentation and Syntax
+#Learn about comments & why to use comments
+
+#Python syntax can be executed by writing directly in the Command Line:print("Hello, World!")  -- Single Line Comment
+""" --Comment Block
+Or by creating a python file, using the .py file extension, and running it in the Command Line:
+python 'A:\\Ashish-GIT\\Python_Training\\Python-Learnings\\Docs\\HandsOn-Labs\\1 HelloWorld.py'
+
+Python Indentation
+
+Indentation refers to the spaces at the beginning of a code line.
+Where in other programming languages the indentation in code is for readability only, the indentation in Python is very important.
+Python uses indentation to indicate a block of code.
+
+Valid Syntax
+
+if 5 > 2:
+  print("Five is greater than two!")
+
+InValid Syntax
+
+
+if 5 > 2:
+print("Five is greater than two!")
+
+The number of spaces is up to you as a programmer, the most common use is four, but it has to be at least one.
+
+"""
+print("Hello, World!")
+if 5 > 2:
+print("Five is greater than two!")
